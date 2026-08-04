e14698ecfdf54ec5f3d50fa4280a81f79738acca
Huzaifa Waheed Khan <sehuzaifa24@gmail.com>
2026-08-04 19:09:48 +0500

    Add main function to load and display alerts

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
+from parser import load_alert
 
+
+def main():
+
+    alert = load_alert("../sample_alerts/ssh_bruteforce.json")
+
+    print("========== WAZUH ALERT ==========\n")
+
+    print(f"Rule ID      : {alert['rule']['id']}")
+    print(f"Rule Level   : {alert['rule']['level']}")
+    print(f"Description  : {alert['rule']['description']}")
+    print(f"Agent        : {alert['agent']['name']}")
+    print(f"Source IP    : {alert['srcip']}")
+    print(f"Timestamp    : {alert['timestamp']}")
+    print(f"MITRE IDs    : {', '.join(alert['mitre']['id'])}")
+    print(f"Tactics      : {', '.join(alert['mitre']['tactic'])}")
+
+
+if __name__ == "__main__":
+    main()
